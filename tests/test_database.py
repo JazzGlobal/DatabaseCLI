83493ec256a01f8489efa9e06b1684d6b58e0758
Christopher Gambrell <waymer147@gmail.com>
2021-06-30 15:19:35 -0400

    - Skip demo test - Added repr generator test for DatabaseProbe

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,11 @@
 import pytest
+from src.database.database import DatabaseProbe
 
-
+@pytest.mark.skip
 def test_database_placeholder():
     assert "" == ""
+
+
+def test_can_generate_database_probe_from_repr(database_probe):
+    result = eval(repr(database_probe))
+    assert isinstance(result, DatabaseProbe)
