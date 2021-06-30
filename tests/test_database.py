ec5f210de5a3cf836bf4a38ee23a9a3d9c600002
Christopher Gambrell <waymer147@gmail.com>
2021-06-30 19:30:23 -0400

    Organization.

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,7 @@
+# Python Imports
 import pytest
 from src.database.database import DatabaseProbe
+
 
 @pytest.mark.skip("This is just an example test.")
 def test_database_placeholder():
@@ -9,3 +11,5 @@ def test_database_placeholder():
 def test_can_generate_database_probe_from_repr(database_probe):
     result = eval(repr(database_probe))
     assert isinstance(result, DatabaseProbe)
+
+
