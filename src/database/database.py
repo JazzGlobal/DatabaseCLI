5a0b549bed24f5431771eac86cf98ffb386a4f02
Christopher Gambrell <waymer147@gmail.com>
2021-06-30 19:30:06 -0400

    - Added execute_query to DatabaseProbe - Refactored click.commands to use DatabaseProbe object.

--- src/database/database.py
+++ src/database/database.py
@@ -1,51 +1,65 @@
+# Python Imports
 import clr
 import click
 import sys
 import os
+from icecream import ic
+
 sys.path.append(os.getcwd() + r"\ref")
 
+# .NET Imports
 clr.AddReference("SqlConnector")
 from SqlConnector import SQLConnector
 
 
 class DatabaseProbe:
     def __init__(self):
-        pass
+        self.sql_connector = SQLConnector("")
+        self.sql_connector.InitializeConnection()
 
     def __repr__(self):
         return "DatabaseProbe()"
 
     def __str__(self):
-        return "DatabaseProbe: No Elements"
+        return f"DatabaseProbe: \nConnection String = {self.sql_connector.ConnectionString}"
+
+    def execute_query(self, sql_query_string):
+        self.sql_connector.Open()
+        command = self.sql_connector.CreateCommand(sql_query_string)
+        reader = self.sql_connector.ReadResults(command)
+
+        result_set = []
+
+        while reader.Read():
+            result_set.append(reader[0])
+
+        self.sql_connector.Close()
+        return result_set
+
+    def dispose(self):
+        self.sql_connector.Dispose()
 
 
 @click.command()
 def get_databases():
     """ Prints the available databases within the SQLEXPRESS server instance. """
-    connector = SQLConnector("")
-    connector.InitializeConnection()
-    connector.Open()
-
-    command = connector.CreateCommand("SELECT * FROM SYS.DATABASES;")
-    reader = connector.ReadResults(command)
-
-    result_set = []
-
-    while reader.Read():
-        result_set.append(reader[0])
-
-    connector.Close()
-    click.echo(result_set)
-    return result_set
+    try:
+        database_probe = DatabaseProbe()
+        result_set = database_probe.execute_query("SELECT * FROM SYS.DATABASES;")
+        click.echo(result_set)
+        return result_set
+    except Exception as e:
+        ic(e)
+    finally:
+        database_probe.dispose()
 
 
 @click.command()
 @click.option("--full-backup", default=False, help="Determines whether or not the command executes a backup over each "
                                                    "database within the server instance.")
 def backup_databases(full_backup):
-    """ Executes a TSQL BACKUP command for the specified databases. """
-    connector = SQLConnector("")
-    connector.InitializeConnection()
+    """ Executes a T-SQL BACKUP command for the specified databases. """
+    database_probe = DatabaseProbe()
 
     if full_backup:
         sql = "SELECT * FROM SYS.DATABASES WHERE NAME NOT IN ('master','model','msdb','tempdb')"
@@ -53,23 +67,16 @@ def backup_databases(full_backup):
         sys.exit("Script exited because only total backups have been implemented. Re-use command with option "
                  "--full-backup True")
 
-    connector.Open()
-    command = connector.CreateCommand(sql)
-    reader = connector.ReadResults(command)
-    result_set = []
-    while reader.Read():
-        result_set.append(reader[0])
-    connector.Close()
-
+    result_set = database_probe.execute_query(sql)
     for database in result_set:
         try:
-            connector.Open()
             sql = f"BACKUP DATABASE \"{database}\" TO DISK = \'C:\\CEMDAS\\BACKUP\\{database}.BAK\' WITH INIT"
             print("Executing: ", sql)
-            backup_command = connector.CreateCommand(sql)
-            connector.ReadResults(backup_command)
+            database_probe = DatabaseProbe()  # Create a new probe for each backup execution. We have to do this
+            # because the queries get pushed to SQL SERVER for execution.
+            database_probe.execute_query(sql)
             print(f"Successfully Backed Up: {database}")
         except Exception as e:
-            print(e)
+            ic(e)
         finally:
-            connector.Close()
+            database_probe.dispose()
